206c712715fc5d9d60ca5f1785b7a48d79790a46
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-22 23:08:14 +0530

    Created seprate Compenents for Pages.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import "../styles/App.css"
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import CheckOut from './pages/CheckOut';
+import Home from './pages/Home';
 
 const App = () => {
 
@@ -19,10 +21,12 @@ useEffect(() => {
     <Router>
       <Switch>
         <Route path="/checkOut">
-          <h1>Check out Page</h1>
+        {/* Created seprate Compenents for Check Out Pages. */}
+          <h1><CheckOut/></h1>
         </Route>
         <Route path="/" exact>
-          <h1>Home Page</h1>
+        {/* Created seprate Compenents for Home Pages. */}
+          <h1><Home/></h1>
         </Route>
       {/* <div>
         {products?JSON.stringify(products):"eerg"}
