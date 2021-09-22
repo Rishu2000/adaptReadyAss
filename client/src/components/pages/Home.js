998258961297ddbc969cd21a69b0aa3a52033074
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-23 00:15:21 +0530

    Render all the products.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,23 +1,29 @@
 import React from 'react'
 
-const Home = () => {
+const Home = ({products}) => {
+    console.log(products);
     return (
         <div className="container">
             <div className="row pt-3">
                 <h1 className="text-center">Products</h1>
-                <div className="pt-3">
-                    <div className="p-2 w-25" style={{backgroundColor:"#b3e0ff", borderRadius:"5px"}}>
-                        <h5>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h5>
-                        <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Product1" width="100%"/>
-                        <div className="pt-2" style={{display: 'flex', justifyContent: 'space-between'}}>
-                            <h6><i>Price:</i> 109.95</h6>
-                            <h6><i>Rating:</i> 3.9(120)</h6>
+                <div className="pt-3" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
+                    {/* Products */}
+                    {products?products.map((product, key) => (
+                        <div key={key} className="p-2 m-2" style={{backgroundColor:"#b3e0ff", borderRadius:"5px", width:"250px"}}>
+                            <h5 style={{height:"70px"}}>{product.title}</h5>
+                            <img src={product.image} alt="Product1" width="100%" height="300px"/>
+                            <div className="pt-2" style={{display: 'flex', justifyContent: 'space-between'}}>
+                                <h6><i>Price:</i> {product.price}</h6>
+                                <h6><i>Rating:</i> {product.rating.rate}({product.rating.count})</h6>
+                            </div>
+                            <div className="pt-2" style={{display: 'flex', justifyContent: 'center'}}>
+                                <button className="btn btn-info" style={{height: '40px'}}>Add to Cart</button>
+                            </div>
                         </div>
-                        <div className="pt-2" style={{display: 'flex', justifyContent: 'center'}}>
-                            <button className="btn btn-info" style={{height: '40px'}}>Add to Cart</button>
-                        </div>
-                        <h6 className="pt-2">Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</h6>
-                    </div>
+                    ))
+                    :(
+                        <h1>No Products found.</h1>
+                    )}
                 </div>
             </div>
         </div>
