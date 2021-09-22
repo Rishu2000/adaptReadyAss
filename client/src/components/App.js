a040b92d21af2fcd345523d7d37cc1bb251c0e00
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-22 22:40:15 +0530

    Get all the json data.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "../styles/App.css"
 
 const App = () => {
+
+const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  const fetchProducts = async () => {
+    const data = await fetch('./products.json');
+    const productsData = await data.json();
+    setProducts(productsData);
+  }
+  fetchProducts();
+},[])
+
   return (
     <div>
-      Rishav
+      {products?JSON.stringify(products):"eerg"}
     </div>
   )
 }
