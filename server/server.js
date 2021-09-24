385305604e1ddf091a1d7632351dc1fd2ae06d81
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 19:35:49 +0530

    Implemented cors.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 4001;
 const login = require('./route/login');
 const register = require('./route/register');
+
+app.use(cors());
 
 app.use('/login',login);
 app.use('/register',register);
