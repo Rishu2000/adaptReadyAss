7ef9773d984e51de4b26c8c8da39abd1c3421c07
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 19:38:39 +0530

    Implemented express-session.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const session = require('express-session');
 const port = 4001;
 const login = require('./route/login');
 const register = require('./route/register');
 
 app.use(cors());
+app.use(session({
+    secret: 'adapt ready project',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use('/login',login);
 app.use('/register',register);
