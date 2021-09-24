55c9d934dee456b6734270aa66bfbd545d675ff6
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 20:26:56 +0530

    Added express.json() middleware to get the post body.

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const login = require('./route/login');
 const register = require('./route/register');
 
 app.use(cors());
+app.use(express.json());
 app.use(session({
     secret: 'adapt ready project',
     resave: false,
