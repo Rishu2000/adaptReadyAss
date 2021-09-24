0e5a4bc330fe426135d9476345c87cfed7537172
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 23:05:41 +0530

    Added backend part for the register.

--- server/route/register.js
+++ server/route/register.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const register = express();
+const users = require('../constants/usersData')
 
 register.get('/', (req, res) => {
-    res.json('You are in register folder.');
+    res.json(users);
 })
 
 register.post('/', (req, res) => {
     const {username, password} = req.body;
+    if(!username || !password){
+        res.status(400).json("Please provide username and password both");
+    }else{
+        if(!users[username]){
+            users[username] = password;
+            res.status(201).json({
+                Success: true,
+                Message:username
+            })
+        }else{
+            res.status(409).json("User already exits.")
+        }
+    }
 })
 
 module.exports = register;
