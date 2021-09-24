6415c672af3688caae4b92518a134915aee01f13
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 20:27:18 +0530

    Completed login functionality.

--- server/route/login.js
+++ server/route/login.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const login = express();
+const users = require('../constants/usersData');
 
 login.get('/', (req, res) => {
-    res.json('You are in login folder.');
+    const {Authentication} = req.session;
+    if(Authentication){
+        res.json(Authentication);
+    }else{
+        res.status(401).json('Not Authenticated.');
+    }
+})
+
+login.post('/', (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password){
+        res.status(400).json("Please provide username and password both");
+    }else{
+        // res.json(users[username]);
+        if(users[username]){
+            if(users[username] === password){
+                req.session.Authentication = req.body;
+                res.status(202).json("Successfully loged in");
+            }else{
+                res.status(400).json("Please enter correct password.");
+            }
+        }else{
+            res.status(404).json("User not found");
+        }
+    }
 })
 
 module.exports = login;
