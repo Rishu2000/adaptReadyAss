09ce0e43c365d7d715ef1be6f46fffafa56c1b61
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-09-24 22:17:06 +0530

    Created login form and added it's functionality.

---
+++ client/src/components/forms/LoginForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import axios from "../axiosCall/axios"
+
+const LoginForm = ({setUser}) => {
+
+const [error, setError] = useState(null);
+const [username, setUsername] = useState(null);
+const [password, setPassword] = useState(null);
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(`/login`,{username,password}).then((res) => {
+        setUser(res.data.user.username)
+        setError(null)
+    }).catch((err) => {
+        setError(err.response.data);
+        setUser(null)
+    })
+}
+
+    return (
+        <div>
+            {error && (
+                <div className="alert alert-danger">{error}</div>
+            )}
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label className="form-label">Email address</label>
+                    <input type="text" className="form-control"
+                        onChange={(e) => setUsername(e.target.value)}
+                        id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" 
+                        onChange={(e) => setPassword(e.target.value)}
+                        id="exampleInputPassword1"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm
